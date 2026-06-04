1651227612bfb78ea489ed669d0338f22d024779
Botirbek <abduxalimovbotirbek229@gmail.com>
2026-06-04 13:30:30 +0500

    Updates: several ORM functions are added in the views.py

--- product/urls.py
+++ product/urls.py
@@ -1,11 +1,26 @@
 from django.urls import path
-from .views import get_product, create_product, read_product, update_product, delete_product
+from product import views
 
 urlpatterns = [
-    path('',get_product, name = 'get-product'),
-    path('create/',create_product, name = 'create'),
-    path('read/<int:pk>/', read_product, name='read'),
-    path('update/<int:pk>/', update_product, name='update'),
-    path('delete/<int:pk>/', delete_product, name='delete'),
+    path('',views.get_product, name = 'get-product'),
+    path('create/',views.create_product, name = 'create'),
+    path('read/<int:pk>/', views.read_product, name='read'),
+    path('update/<int:pk>/', views.update_product, name='update'),
+    path('delete/<int:pk>/', views.delete_product, name='delete'),
+    path('filter/',views.get_over_500,name='over-500'),
+    path('first/', views.get_first, name='first'),
+    path('last/', views.get_last, name='last'),
+    path('reverse/', views.get_reverse, name='reverse'),
+    path('quantity/', views.count_products, name='quantity'),
+    path('existence/', views.check_existence, name='existence'),
+    path('none/', views.get_none, name='none'),
+    path('distinct/', views.get_distinct, name='distinct'),
+    path('dict/', views.get_dict, name='dict'),
+    path('tuple/', views.get_tuple, name='tuple'),
+    path('list/', views.get_list, name='list'),
+    path('only/', views.get_only, name='only'),
+    path('defer/', views.defer, name='defer'),
+    path('iterator/', views.get_iterator, name='iterator'),
+    path('raw/', views.get_raw, name='raw'),
 ]
 
--- product/views.py
+++ product/views.py
@@ -3,8 +3,74 @@ from .models import Product
 
 # Create your views here.
 def get_product(request):
-    pro = Product.objects.all()
+    pro = Product.objects.all().order_by('-price')
     return render(request, 'product/list.html',{'pro':pro})
+
+def get_over_500(request):
+    pro = Product.objects.filter(price__gte=500).order_by('price')
+    return render(request, 'product/list.html', {'pro': pro})
+
+def get_first(request):
+    pro = Product.objects.first()
+    return render(request, 'product/read.html',{'pro':pro})
+
+def get_last(request):
+    pro = Product.objects.last()
+    return render(request, 'product/read.html',{'pro':pro})
+
+def get_reverse(request):
+    pro = Product.objects.reverse()
+    return render(request,'product/list.html',{'pro':pro})
+
+def count_products(request):
+    quantity = Product.objects.count()
+    return render(request, 'product/data.html',{'quantity':quantity})
+
+def check_existence(request):
+    existence = Product.objects.exists()
+    return render(request,'product/data.html',{'existence':existence})
+
+def get_none(request):
+    none = Product.objects.none()
+    return render(request,'product/data.html',{'none':none})
+
+def get_distinct(request):
+    pro = Product.objects.distinct()
+    return render(request,'product/list.html',{'pro':pro})
+
+def get_dict(request):
+    pro_dict = Product.objects.values()
+    return render(request,'product/data.html',{'pro_dict':pro_dict})
+
+def get_tuple(request):
+    pro_tuple = Product.objects.values_list()
+    return render(request,'product/data.html',{'pro_tuple':pro_tuple})
+
+def get_list(request):
+    pro_list = Product.objects.values_list('title',flat=True)
+    return render(request,'product/data.html',{'pro_list':pro_list})
+
+def get_only(request):
+    pro_only = Product.objects.only('title','price')
+    return render(request,'product/data.html',{'pro_only':pro_only})
+
+def defer(request):
+    pro_defer = Product.objects.defer('created_at')
+    return render(request,'product/data.html',{'pro_defer':pro_defer})
+
+def get_iterator(request):
+    pro_iterator = Product.objects.defer('created_at')
+    return render(request,'product/data.html',{'pro_iterator':pro_iterator})
+
+def get_raw(request):
+    pro_raw = Product.objects.raw('select * from product_product where price > 1000')
+    return render(request,'product/data.html',{'pro_raw':pro_raw})
+
+
+
+
+
+
 
 def create_product(request):
     if request.method == "POST":
@@ -34,11 +100,13 @@ def update_product(request, pk):
         price = request.POST.get('price',pro.price)
         quantity = request.POST.get('quantity',pro.quantity)
         description = request.POST.get('description',pro.description)
-        if title and price and quantity and description:
+        updated_at = request.POST.get('updated_at',pro.updated_at)
+        if title and price and quantity and description and updated_at:
             pro.title = title
             pro.price = price
             pro.quantity = quantity
             pro.description = description
+            pro.updated_at = updated_at
             pro.save()
             return redirect('get-product')
     else:
